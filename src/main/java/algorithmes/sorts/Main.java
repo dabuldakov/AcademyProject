9a83bc8aecba887479ca6b472a7c5f85d5043733
DmitryBuldakov <dmitry.buldakov@syncretis.com>
2021-06-29 13:38:58 +0700

    moved to utils sort methods

---
+++ src/main/java/algorithmes/sorts/Main.java
@@ -0,0 +1,25 @@
+package algorithmes.sorts;
+
+import algorithmes.sorts.sortImpl.BubbleSort;
+import algorithmes.sorts.sortImpl.QuickSort;
+import algorithmes.sorts.sortImpl.SelectionSort;
+import algorithmes.sorts.sortInterface.Sort;
+import algorithmes.sorts.util.UtilsStudy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = UtilsStudy.prepareArray(7);
+        int[] array2 = UtilsStudy.copyArray(array);
+        int[] array3 = UtilsStudy.copyArray(array);
+
+        Sort quickSort = new QuickSort(array);
+        Sort bubbleSort = new BubbleSort(array2);
+        Sort selectionSort = new SelectionSort(array3);
+        UtilsStudy.printSort(quickSort);
+        UtilsStudy.printSort(bubbleSort);
+        UtilsStudy.printSort(selectionSort);
+
+    }
+
+}
--- src/main/java/algorithmes/sorts/util/UtilsStudy.java
+++ src/main/java/algorithmes/sorts/util/UtilsStudy.java
@@ -1,5 +1,7 @@
 package algorithmes.sorts.util;
 
+import algorithmes.sorts.sortInterface.Sort;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
@@ -8,7 +10,7 @@ public class UtilsStudy {
 
     public static ArrayList<Integer> createArrayList() {
 
-        ArrayList<Integer> array = new ArrayList<Integer>();
+        ArrayList<Integer> array = new ArrayList<>();
         Random random = new Random();
         int upperbound = 100000;
         for (int i = 1; i <= 100; i++) {
@@ -19,7 +21,7 @@ public class UtilsStudy {
 
     public static LinkedList<Integer> createLinkedList(int size) {
 
-        LinkedList<Integer> list = new LinkedList<Integer>();
+        LinkedList<Integer> list = new LinkedList<>();
         Random random = new Random();
         int upperbound = 100000;
         for (int i = 1; i <= size; i++) {
@@ -28,4 +30,28 @@ public class UtilsStudy {
         return list;
     }
 
+    public static void printSort(Sort sort) {
+        sort.print();
+        long startTime = System.currentTimeMillis();
+        sort.sort(sort.getArray());
+        long endTime = System.currentTimeMillis();
+        sort.print();
+        System.out.println(sort.getName() + ". Total execution time array: " + (endTime - startTime) + "ms.");
+        System.out.println("---------------------");
+    }
+
+    public static int[] prepareArray(int size) {
+        int[] array = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(size);
+        }
+        return array;
+    }
+
+    public static int[] copyArray(int[] array) {
+        int[] copy = new int[array.length];
+        System.arraycopy(array, 0, copy, 0, array.length);
+        return copy;
+    }
 }
