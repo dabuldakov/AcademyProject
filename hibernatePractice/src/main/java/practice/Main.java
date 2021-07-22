867c44b41ca0f16847772f805836303de395fa63
DmitryBuldakov <dmitry.buldakov@syncretis.com>
2021-07-22 18:51:39 +0700

    Hibernate CRUD

---
+++ hibernatePractice/src/main/java/practice/Main.java
@@ -0,0 +1,43 @@
+package practice;
+
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+    public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static void main(String[] args) throws ParseException {
+        PersonDAO personDAO = new PersonDAO();
+        Person person = new Person();
+
+        //Find person
+//        person = personDAO.findPerson(15);
+//        System.out.println(person);
+
+        //Update person
+//        person.setFirstName("Oleg aka Hibernate");
+//        Person person1 = personDAO.updatePerson(person);
+//        System.out.println(person1);
+
+        //Delete person
+//        person.setId(60);
+//        boolean b = personDAO.deletePerson(person);
+//        System.out.println(b);
+
+        //Save person
+        person.setFirstName("Olya");
+        person.setSecondName("Frolova");
+        person.setBirthday(DATE_FORMAT.parse("1983-02-14"));
+        Department department = new Department();
+        department.setId(1);
+        department.setName("Java developers department");
+        person.setDepartment(department);
+        boolean b = personDAO.savePerson(person);
+        System.out.println(b);
+
+
+    }
+}
---
+++ hibernatePractice/src/main/java/practice/PersonDAO.java
@@ -0,0 +1,49 @@
+package practice;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.StaleStateException;
+import org.hibernate.Transaction;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
+
+import javax.persistence.OptimisticLockException;
+
+public class PersonDAO {
+
+    public Person findPerson(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Person.class, id);
+    }
+
+    public Person updatePerson(Person person) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(person);
+        transaction.commit();
+        return findPerson(person.getId());
+    }
+
+    public boolean deletePerson(Person person) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(person);
+        try {
+            transaction.commit();
+        }catch (OptimisticLockException e){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean savePerson(Person person){
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(person);
+        try {
+            transaction.commit();
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+}
